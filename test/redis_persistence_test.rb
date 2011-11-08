11efa24ed32b688f6b27e39778b40c792dfce2ff
Karel Minarik <karmi@karmi.cz>
2011-11-08 20:58:04 +0100

    Added simple test for persistence

--- test/redis_persistence_test.rb
+++ test/redis_persistence_test.rb
@@ -2,6 +2,9 @@ require '_helper'
 
 class PersistentArticle
   include Redis::Persistence
+
+  property :id
+  property :title
 end
 
 class RedisPersistenceTest < ActiveSupport::TestCase
@@ -27,6 +30,13 @@ class RedisPersistenceTest < ActiveSupport::TestCase
 
     should "not persist by default" do
       assert ! PersistentArticle.new.persisted?
+    end
+
+    should "be saved and found in Redis" do
+      article = PersistentArticle.new id: 1, title: 'One'
+      assert article.save
+      assert PersistentArticle.find(1)
+      assert_equal 'One', PersistentArticle.find(1).title
     end
 
   end
