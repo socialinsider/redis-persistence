772c75e5a78d37f5ffe0bb57629dcfbe785d32f4
Karel Minarik <karmi@karmi.cz>
2011-11-08 18:57:04 +0100

    Added basic test infrastructure

---
+++ test/redis_persistence_test.rb
@@ -0,0 +1,19 @@
+require '_helper'
+
+module Redis
+  module Persistence
+
+    class RedisPersistenceTest < Test::Unit::TestCase
+
+      context "Basically..." do
+
+        should "work..." do
+          assert true
+        end
+
+      end
+
+    end
+
+  end
+end
