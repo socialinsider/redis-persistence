a5d9e4523bc95566b5791bab5952e070e33c0c19
Karel Minarik <karmi@karmi.cz>
2011-11-08 19:03:54 +0100

    Added basic draft of ActiveModel compatibility

---
+++ test/active_model_lint_test.rb
@@ -0,0 +1,21 @@
+require '_helper'
+
+class PersistentArticle
+  include Redis::Persistence
+end
+
+class Redis
+  module Persistence
+
+    class ActiveModelLintTest < Test::Unit::TestCase
+
+      include ActiveModel::Lint::Tests
+
+      def setup
+        @model = PersistentArticle.new :title => 'Test'
+      end
+
+    end
+
+  end
+end
