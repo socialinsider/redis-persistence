ff4b1731c86401a93b9f71c7a9698b630655b44b
Karel Minarik <karmi@karmi.cz>
2011-11-08 22:32:39 +0100

    Added callbacks implementation and tests

--- test/models.rb
+++ test/models.rb
@@ -11,3 +11,16 @@ class ModelWithDefaults
   property :title, default: '(Unknown)'
   property :admin, default: true
 end
+
+class ModelWithCallbacks
+  include Redis::Persistence
+
+  before_save    :my_callback_method
+  after_save     :my_callback_method
+  before_destroy { @hooked = 'YEAH' }
+
+  property :title
+
+  def my_callback_method
+  end
+end
